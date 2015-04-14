e024c171591eed1ecffa485a36aec4d750cda8ac
Ron Gebauer <mail@ron.gebauers.org>
2015-04-14 13:14:53 +0200

    Change ExerciseData bz class diagram

--- app/src/main/java/exercise/fontys/nl/exercisecontrolbackend/ExerciseData.java
+++ app/src/main/java/exercise/fontys/nl/exercisecontrolbackend/ExerciseData.java
@@ -1,54 +1,38 @@
 package exercise.fontys.nl.exercisecontrolbackend;
 
-import java.util.Date;
-
 /**
  * Created by root on 08.04.15.
- *
- * stores the data that is collected from the excercises
+ * <p/>
+ * stores the data that is collected from the exercises
  */
-public class ExerciseData {
-    private Date timeStamp;
+public class ExerciseData
+{
     private String typeOfExercise;
-    private PartialExerciseData accelData;
-    private PartialExerciseData gyroData;
+    private SingleExerciseData singleExerciseData;
 
-    public ExerciseData(Date timeStamp, String typeOfExercise, PartialExerciseData accelData, PartialExerciseData gyroData) {
-        this.timeStamp = timeStamp;
+    public ExerciseData(String typeOfExercise, SingleExerciseData singleExerciseData)
+    {
         this.typeOfExercise = typeOfExercise;
-        this.accelData = accelData;
-        this.gyroData = gyroData;
+        this.singleExerciseData = singleExerciseData;
     }
 
-    public Date getTimeStamp() {
-        return timeStamp;
-    }
-
-    public void setTimeStamp(Date timeStamp) {
-        this.timeStamp = timeStamp;
-    }
-
-    public String getTypeOfExercise() {
+    public String getTypeOfExercise()
+    {
         return typeOfExercise;
     }
 
-    public void setTypeOfExercise(String typeOfExercise) {
+    public void setTypeOfExercise(String typeOfExercise)
+    {
         this.typeOfExercise = typeOfExercise;
     }
 
-    public PartialExerciseData getAccelData() {
-        return accelData;
+    public SingleExerciseData singleExerciseData()
+    {
+        return singleExerciseData;
     }
 
-    public void setAccelData(PartialExerciseData accelData) {
-        this.accelData = accelData;
-    }
-
-    public PartialExerciseData getGyroData() {
-        return gyroData;
-    }
-
-    public void setGyroData(PartialExerciseData gyroData) {
-        this.gyroData = gyroData;
+    public void setGyroData(SingleExerciseData singleExerciseData)
+    {
+        this.singleExerciseData = singleExerciseData;
     }
 }
