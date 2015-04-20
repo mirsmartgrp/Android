9f900bd00db395edcfe25043b61b6b04455860f0
ko08ok <m.walter@student.fontys.nl>
2015-04-20 12:26:03 +0200

    removed merge conflicts

--- mobile/src/main/java/nl/fontys/exercise/MainActivityMobile.java
+++ mobile/src/main/java/nl/fontys/exercise/MainActivityMobile.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import nl.fontys.connection.ConnectionHandlerBackend;
+import nl.fontys.listener.Listener;
 
 
 public class MainActivityMobile extends Activity
