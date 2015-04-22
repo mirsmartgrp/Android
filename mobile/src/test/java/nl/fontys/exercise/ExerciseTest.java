4453970a49fbab16840aee232863633b417aeb22
Techlogic <nils.heyer@gmx.de>
2015-04-22 12:51:56 +0200

    Added test

---
+++ mobile/src/test/java/nl/fontys/exercise/ExerciseTest.java
@@ -0,0 +1,33 @@
+package nl.fontys.exercise;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExerciseTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testParseExercise() throws Exception {
+
+    int i = 1;
+    int j = 1;
+
+     assertEquals(i+j,2);
+
+
+    }
+}
