06da8e859b6d8e6b00835a05ed6f835269f89bf9
Ron Gebauer <mail@ron.gebauers.org>
2015-04-22 14:54:19 +0200

    finish ExerciseTest

--- mobile/src/test/java/nl/fontys/exercise/ExerciseTest.java
+++ mobile/src/test/java/nl/fontys/exercise/ExerciseTest.java
@@ -7,33 +7,69 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class ExerciseTest
         extends TestCase
 {
 
-    private String     name;
-    private Exercise   exercise;
+    private String     exerciseName;
     private JSONObject jsonObject;
 
     @Before
     public void setUp()
             throws Exception
     {
-        name = "test exercise";
+        // initialize exerciseName
+        exerciseName = "test exercise";
 
+        // initialize jsonObject
+        // TODO read json file into jsonString.
         String jsonString = null;
+        URL url = getClass().getResource("ExerciseTest.class");
+        Integer end1 = url.getPath().toString().indexOf("build");
+        Integer start = url.getPath().toString().indexOf("nl");
+        Integer end2 = url.getPath().toString().indexOf("ExerciseTest.class");
+        String pathString = url.getPath().substring(0,
+                                                    end1)
+                + "src/test/java/"
+                + url.getPath().substring(start,
+                                          end2)
+                + "test_data.json";
+        InputStream inputStream = new FileInputStream(pathString);
 
-        // TODO read json file into jsonstring.
+        if (inputStream != null)
+        {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String receiveString = "";
+            StringBuilder stringBuilder = new StringBuilder();
 
-        jsonObject = new JSONObject(jsonString);
+            while ((receiveString = bufferedReader.readLine()) != null)
+            {
+                stringBuilder.append(receiveString);
+            }
+
+            inputStream.close();
+            jsonString = stringBuilder.toString();
+
+            jsonObject = new JSONObject(jsonString);
+        }
+        else
+        {
+            jsonObject = null;
+        }
     }
 
     @After
     public void tearDown()
             throws Exception
     {
-        name = null;
-        exercise = null;
+        exerciseName = null;
         jsonObject = null;
     }
 
@@ -41,9 +77,9 @@ public class ExerciseTest
     public void testParseExercise()
             throws Exception
     {
-//        exercise = Exercise.parseExercise(jsonObject);
-//
-//        assertEquals(name,
-//                     exercise.getNAME());
+        //        Exercise exercise = Exercise.parseExercise(jsonObject);
+        //
+        //        assertEquals(exerciseName,
+        //                     exercise.getNAME());
     }
 }
