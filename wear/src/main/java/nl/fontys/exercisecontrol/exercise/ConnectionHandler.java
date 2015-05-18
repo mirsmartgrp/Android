fbea9968d6bd11e8449c1a4f3dbd338197af5253
ko08ok <m.walter@student.fontys.nl>
2015-05-18 11:38:48 +0200

    added possibility to receive messages

--- wear/src/main/java/nl/fontys/exercisecontrol/exercise/ConnectionHandler.java
+++ wear/src/main/java/nl/fontys/exercisecontrol/exercise/ConnectionHandler.java
@@ -1,6 +1,7 @@
 package nl.fontys.exercisecontrol.exercise;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -11,6 +12,13 @@ import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
+
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by max on 16.04.15.
@@ -23,6 +31,7 @@ public class ConnectionHandler implements GoogleApiClient.ConnectionCallbacks {
     private static final String WEAR_MESSAGE_PATH = "/system/exercise";
     private GoogleApiClient mApiClient ;
     private Context context;
+    public final static String FEEDBACK="nl.fontys.exercisecontrol.exercise.Feedback";
 
     public ConnectionHandler(Context context) {
         this.context=context;
@@ -43,9 +52,89 @@ public class ConnectionHandler implements GoogleApiClient.ConnectionCallbacks {
         Wearable.MessageApi.addListener(mApiClient, new MessageApi.MessageListener() {
             @Override
             public void onMessageReceived(MessageEvent messageEvent) {
-            Log.d(this.getClass().getName(), "message received " + messageEvent.getData().toString());
+                Log.d(this.getClass().getName(), "message received " + messageEvent.getData().toString());
+                if (isFeedback(messageEvent)) {
+                    handleFeedback(messageEvent);
+                } else if (isDownload(messageEvent)) {
+                    handleDownload(messageEvent);
+                } else {
+                    Log.d(this.getClass().getName(), "unknown message " + messageEvent);
+                }
             }
         });
+    }
+
+    /**
+     * Download exercise list
+     */
+    private void handleDownload(MessageEvent event) {
+            BufferedInputStream in = null;
+            FileOutputStream fout = null;
+            String urlString="";
+            String filename="";
+            try {
+                in = new BufferedInputStream(new URL(urlString).openStream());
+                fout = new FileOutputStream(filename);
+
+                final byte data[] = new byte[1024];
+                int count;
+                while ((count = in.read(data, 0, 1024)) != -1) {
+                    fout.write(data, 0, count);
+                }
+            } catch (MalformedURLException e) {
+                Log.e(this.getClass().getName(), "no valid url "+e);
+            } catch (FileNotFoundException e) {
+               Log.e(this.getClass().getName(),"file not found "+e);
+            } catch (IOException e) {
+                Log.e(this.getClass().getName(),"error reading file "+e);
+            } finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        Log.e(this.getClass().getName(), "error closing BufferedInputStream "+e);
+
+                    }
+                }
+                if (fout != null) {
+                    try {
+                        fout.close();
+                    } catch (IOException e) {
+                        Log.e(this.getClass().getName(), "error closing FileOutputStream " + e);
+
+                    }
+                }
+            }
+    }
+
+    /**
+     * show feedback message
+     * @param messageEvent
+     */
+    private void handleFeedback(MessageEvent messageEvent) {
+        //TODO impl
+        Intent i=new Intent(context,MessageViewActivity.class);
+        i.putExtra(FEEDBACK, messageEvent.getData() );
+        context.startActivity(i);
+    }
+
+    /**
+     *
+     * @param messageEvent message received
+     * @return true if message indicates to download exercises
+     */
+    private boolean isDownload(MessageEvent messageEvent) {
+        return messageEvent.getData().toString().contains("feedback");
+
+    }
+
+    /**
+     * TODO: check with real Strings
+     * @param messageEvent messsage received
+     * @return true if messae contains feedback for exercises
+     */
+    private boolean isFeedback(MessageEvent messageEvent) {
+        return messageEvent.getData().toString().contains("GUID");
     }
 
 
