ee53d733001fcf03e4ed8e06795a1cf49f16480c
Techlogic <nils.heyer@gmx.de>
2015-05-21 12:52:29 +0200

    Changed learnSequence

--- mobile/src/main/java/nl/fontys/exercisecontrol/exercise/Exercise.java
+++ mobile/src/main/java/nl/fontys/exercisecontrol/exercise/Exercise.java
@@ -2,37 +2,24 @@ package nl.fontys.exercisecontrol.exercise;
 
 import android.util.Log;
 
-import com.google.android.gms.games.internal.constants.TimeSpan;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 import be.ac.ulg.montefiore.run.jahmm.Hmm;
 import be.ac.ulg.montefiore.run.jahmm.ObservationVector;
-import be.ac.ulg.montefiore.run.jahmm.OpdfFactory;
-import be.ac.ulg.montefiore.run.jahmm.OpdfMultiGaussianFactory;
-import nl.fontys.exercisecontrol.MainActivityMobile;
 
 /**
  * Created by Ron Gebauer on 20.04.15.
  * <p/>
  * the exercise itself.
  */
-public class Exercise
-{
-    private final String       GUID;
-    private final String       NAME;
-    private final String       DESCRIPTION;
+public class Exercise {
+    private final String GUID;
+    private final String NAME;
+    private final String DESCRIPTION;
     private final ExerciseData EXERCISE_DATA;
     private Hmm<ObservationVector> hmm;
     private List<String> sequences;
@@ -43,55 +30,49 @@ public class Exercise
      *
      * @param name the NAME of the exercise
      */
-    Exercise(String name)
-    {
+    Exercise(String name) {
         this.GUID = "";
         this.NAME = name;
         this.DESCRIPTION = "";
         this.EXERCISE_DATA = new ExerciseData();
-        this.hmm =  null;
+        this.hmm = null;
         this.sequences = null;
     }
 
-    public String getGUID(){
+    public String getGUID() {
         return getGUID();
     }
 
 
-    public void  setHmm(Hmm<ObservationVector> hmm){
+    public void setHmm(Hmm<ObservationVector> hmm) {
         this.hmm = hmm;
     }
 
-    public void setSequences(List<String> sequences){
+    public void setSequences(List<String> sequences) {
         this.sequences = sequences;
     }
 
-    public Hmm<ObservationVector>getHmm(){
-        return  hmm;
+    public Hmm<ObservationVector> getHmm() {
+        return hmm;
     }
 
-    public List<String> getSequences(){
+    public List<String> getSequences() {
         return sequences;
     }
 
 
-
-    public static Exercise parseExercise(JSONObject jsonObject)
-    {
+    public static Exercise parseExercise(JSONObject jsonObject) {
         Exercise exercise = null;
 
-        try
-        {
-            if (jsonObject.length() <= 0)
-            {
+        try {
+            if (jsonObject.length() <= 0) {
                 throw new NullPointerException("jsonObject is empty");
             }
 
             exercise = new Exercise(jsonObject.getString("name"));
 
             JSONArray jsonArray = jsonObject.getJSONArray("data");
-            for (int index = 0; index < jsonArray.length(); index++)
-            {
+            for (int index = 0; index < jsonArray.length(); index++) {
                 JSONObject tmpJsonObject = jsonArray.getJSONObject(index);
                 Double time = tmpJsonObject.getDouble("time");
                 JSONObject accelerometer = tmpJsonObject.getJSONObject("accelerometer");
@@ -110,30 +91,26 @@ public class Exercise
                 };
 
                 exercise.getEXERCISE_DATA().addSingleExerciseData(time,
-                                                                  coordinates);
+                        coordinates);
             }
-        }
-        catch (JSONException e)
-        {
+        } catch (JSONException e) {
             Log.e("parseExercise",
-                  e.getMessage());
+                    e.getMessage());
         }
 
         return exercise;
     }
 
 
-    public String getNAME()
-    {
+    public String getNAME() {
         return NAME;
     }
-    public String getDESCRIPTION()
-    {
+
+    public String getDESCRIPTION() {
         return DESCRIPTION;
     }
 
-    public ExerciseData getEXERCISE_DATA()
-    {
+    public ExerciseData getEXERCISE_DATA() {
         return EXERCISE_DATA;
     }
 }
